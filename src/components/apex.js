8c06dc0136b5c157c78d6cbe7bfc26753414974e
sanyog42 <sanyoggupta42@gmail.com>
2020-04-21 16:43:24 +0530

    Added national events

--- src/components/apex.js
+++ src/components/apex.js
@@ -10,6 +10,108 @@ export default class ApexChart extends React.Component {
 			
             series: [],
             options: {
+			  annotations: {
+				xaxis: [
+				  {
+					x: new Date("03/24/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#eb4034",
+					label: {
+					  borderColor: "#eb4034",
+					  style: {
+						color: "#fff",
+						background: "#eb4034",
+						fontSize: "8px"
+					  },
+					  text: "Lockdown Announced"
+					},
+				  },
+				  {
+					x: new Date("03/10/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#4aed18",
+					label: {
+					  borderColor: "#4aed18",
+					  style: {
+						color: "#fff",
+						background: "#4aed18",
+						fontSize: "8px"
+					  },
+					  text: "First COVID-related Death"
+					},
+				  },
+				  {
+					x: new Date("03/19/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#f2ee0a",
+					label: {
+					  borderColor: "#f2ee0a",
+					  style: {
+						color: "#fff",
+						background: "#f2ee0a",
+						fontSize: "8px",
+					  },
+					  text: "Announcement of Janta Curfew"
+					},
+				  },
+				  {
+					x: new Date("03/22/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#7a6bed",
+					label: {
+					  borderColor: "#7a6bed",
+					  style: {
+						color: "#fff",
+						background: "#7a6bed",
+						fontSize: "8px",
+					  },
+					  text: "Junta Curfew Observed"
+					},
+				  },
+				  {
+					x: new Date("04/03/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#ff9100",
+					label: {
+					  borderColor: "#ff9100",
+					  style: {
+						color: "#fff",
+						background: "#ff9100",
+						fontSize: "8px",
+					  },
+					  text: "Announcement of Diya Jalao"
+					},
+				  },
+				  {
+					x: new Date("04/05/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#ff00d0",
+					label: {
+					  borderColor: "#ff00d0",
+					  style: {
+						color: "#fff",
+						background: "#ff00d0",
+						fontSize: "8px",
+					  },
+					  text: "Diya Jalao Observed at 9PM"
+					},
+				  },
+				  {
+					x: new Date("04/14/2020").getTime(),
+					strokeDashArray: 0,
+					borderColor: "#775DD0",
+					label: {
+					  borderColor: "#775DD0",
+					  style: {
+						color: "#fff",
+						background: "#775DD0",
+						fontSize: "8px",
+					  },
+					  text: "Announcement of Lockdown Extension"
+					},
+				  },
+				],
+			  },
               chart: {
                 type: 'area',
                 stacked: false,
